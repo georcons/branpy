143eeb3a8bead31c6f4f2c05c5126ebe5b423632
unknown <nikolay_yg@protonmail.com>
2025-03-29 15:06:44 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'branpy',
-    version = "1.0.0",
+    version = "1.1.0",
     author = 'Nikolay Georgiev',
     description = 'A package for classifying math problem domains.',
     packages = find_packages(),
-    install_requires = ["openai >= 1.58.1"]
+    install_requires = ["scikit-learn >= 1.5.2", "openai >= 1.58.1"]
 )
