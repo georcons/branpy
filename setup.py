ff652eaa193a74c70387712a4cf778ed9db2a145
Nikolay Georgiev <163644150+georcons@users.noreply.github.com>
2025-03-29 15:14:54 +0200

    Update setup.py
    fixed imports version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'branpy',
-    version = "1.1.0",
+    version = "1.1.1",
     author = 'Nikolay Georgiev',
     description = 'A package for classifying math problem domains.',
     packages = find_packages(),
